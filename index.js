be47497c12fdca54979b87bbca42dc02fd13af60
cd <146971793+chetnadhir@users.noreply.github.com>
2024-02-18 23:38:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
-let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
@@ -86,5 +85,3 @@ const checkWinner = () => {
     }
   }
 };
-
-newGameBtn.addEventListener("click", resetGame);
